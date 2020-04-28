d7af7f835a844809ea3f1fce37ec366e68bc64fa
ansroid <ansledozie@gmail.com>
2020-04-28 15:03:04 +0100

    second commit

--- app/src/main/java/com/example/travel/HomePage.java
+++ app/src/main/java/com/example/travel/HomePage.java
@@ -1,21 +1,34 @@
 package com.example.travel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        ImageButton imageButton = (ImageButton) findViewById(R.id.date_btn);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(),"date picker");
+            }
+        });
 
 
         btn = (Button) findViewById(R.id.find_bus);
@@ -28,6 +41,11 @@ public class HomePage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
 
     }
 }
